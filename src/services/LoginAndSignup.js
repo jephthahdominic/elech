236e85cbf877a59a1f7d3836a71a74f4b88318fa
Jephthah Dominic <stdominicjephthah63@gmail.com>
2025-03-30 21:54:07 +0100

    added the email verification feature

--- src/services/LoginAndSignup.js
+++ src/services/LoginAndSignup.js
@@ -5,7 +5,7 @@ const auth = getAuth();
 const actionCodeSettings = {
     // URL you want to redirect back to. The domain (www.example.com) for this
     // URL must be in the authorized domains list in the Firebase Console.
-    url: 'https://www.example.com/finishSignUp?cartId=1234',
+    url: 'https://elech.vercel.app/',
     // This must be true.
     handleCodeInApp: true,
 };
