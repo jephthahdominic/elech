422f6167b50b215469168306943c2a2fa2d43d60
Jephthah Dominic <stdominicjephthah63@gmail.com>
2025-04-16 13:18:32 +0100

    Worked on payment, added a products page

--- src/ui/NavBar.jsx
+++ src/ui/NavBar.jsx
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom'
 
 export default function NavBar() {
   return (
-    <nav className="w-full flex items-center justify-between gap-3 overflow-x-scroll flex-nowrap verticalNav">
+    <nav className="w-full flex items-center justify-between gap-2 overflow-x-scroll flex-nowrap verticalNav">
         <Link className="text-[1.1rem] text-darkTransparent px-5 py-1 border-2 border-[125,125,125,0.8] rounded-full">
             All
         </Link>
