6115d7cc6077bd904b9b38f6bfb919f5337a55c1
Jephthah Dominic <stdominicjephthah63@gmail.com>
2025-04-16 13:16:07 +0100

    Worked on payment, added a products page

--- src/ui/NavBar.jsx
+++ src/ui/NavBar.jsx
@@ -3,16 +3,16 @@ import { Link } from 'react-router-dom'
 export default function NavBar() {
   return (
     <nav className="w-full flex items-center justify-between gap-5 overflow-x-scroll flex-nowrap verticalNav">
-        <Link className="text-[1.1rem] text-darkTransparent px-5 py-1 border border-[125,125,125,0.8] rounded-full">
+        <Link className="text-[1.1rem] text-darkTransparent px-5 py-1 border-2 border-[125,125,125,0.8] rounded-full">
             All
         </Link>
-        <Link className="text-[1.1rem] text-darkTransparent px-5 py-1 border border-[125,125,125,0.6] rounded-full">
+        <Link className="text-[1.1rem] text-darkTransparent px-5 py-1 border-2 border-[125,125,125,0.6] rounded-full">
             Shoes
         </Link> 
-        <Link className="text-[1.1rem] text-darkTransparent px-5 py-1 border border-[125,125,125,0.6] rounded-full">
+        <Link className="text-[1.1rem] text-darkTransparent px-5 py-1 border-2 border-[125,125,125,0.6] rounded-full">
             Sandals
         </Link>
-        <Link className="text-[1.1rem] text-darkTransparent px-5 py-1 border border-[125,125,125,0.6] rounded-full">
+        <Link className="text-[1.1rem] text-darkTransparent px-5 py-1 border-2 border-[125,125,125,0.6] rounded-full">
             Belts
         </Link>
     </nav>
